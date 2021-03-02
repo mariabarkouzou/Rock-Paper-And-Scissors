8028ff1332cf1c30ab0968e2d4fa890f21641a42
Maria Barkouzou <mbarkouzou@gmail.com>
2021-03-02 11:21:11 +0200

    Fireeeeeeeeeeeeeeee

--- rps.js
+++ rps.js
@@ -1,8 +1,9 @@
- const game = () => {
+const game = () => {
   let pScore = 0;
   let cScore = 0;
 
   //Start the Game
+
   const startGame = () => {
     const playBtn = document.querySelector(".intro button");
     const introScreen = document.querySelector(".intro");
@@ -13,24 +14,29 @@
       match.classList.add("fadeIn");
     });
   };
+
   //Play Match
+
   const playMatch = () => {
     const options = document.querySelectorAll(".options button");
     const playerHand = document.querySelector(".player-hand");
     const computerHand = document.querySelector(".computer-hand");
     const hands = document.querySelectorAll(".hands img");
 
-    hands.forEach(hand => {
-      hand.addEventListener("animationend", function() {
+    hands.forEach((hand) => {
+      hand.addEventListener("animationend", function () {
         this.style.animation = "";
       });
     });
+
     //Computer Options
+
     const computerOptions = ["rock", "paper", "scissors"];
 
-    options.forEach(option => {
-      option.addEventListener("click", function() {
+    options.forEach((option) => {
+      option.addEventListener("click", function () {
         //Computer Choice
+
         const computerNumber = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[computerNumber];
 
@@ -63,10 +69,12 @@
       winner.textContent = "It's a tie!";
       return;
     }
+
     //Check for Rock
+
     if (playerChoice === "rock") {
       if (computerChoice === "scissors") {
-        winner.textContent = "Player Wins!";
+        winner.textContent = "You Win!🔥";
         pScore++;
         updateScore();
         return;
@@ -77,7 +85,9 @@
         return;
       }
     }
+
     //Check for Paper
+
     if (playerChoice === "paper") {
       if (computerChoice === "scissors") {
         winner.textContent = "Computer Wins!";
@@ -85,13 +95,15 @@
         updateScore();
         return;
       } else {
-        winner.textContent = "Player Wins!";
+        winner.textContent = "You Win!🔥";
         pScore++;
         updateScore();
         return;
       }
     }
+
     //Check for Scissors
+
     if (playerChoice === "scissors") {
       if (computerChoice === "rock") {
         winner.textContent = "Computer Wins!";
@@ -99,7 +111,7 @@
         updateScore();
         return;
       } else {
-        winner.textContent = "Player Wins!";
+        winner.textContent = "You Win!🔥";
         pScore++;
         updateScore();
         return;
@@ -107,15 +119,14 @@
     }
   };
 
+  //Call all the inner functions
 
-
-  //Is call all the inner function
   startGame();
   playMatch();
 };
 
+//Start the game function
 
-//start the game function
 game();
 
 // for (let round = 0; round < 5; round++) {
